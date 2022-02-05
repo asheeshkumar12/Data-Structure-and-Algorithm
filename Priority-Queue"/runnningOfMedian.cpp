23d4d1c92bb8859f8cb0573c99c50826b48fc27e
asheeshkumar12 <91361484+asheeshkumar12@users.noreply.github.com>
2022-02-05 12:57:37 +0530

    Update runnningOfMedian.cpp

--- Priority-Queue"/runnningOfMedian.cpp
+++ Priority-Queue"/runnningOfMedian.cpp
@@ -36,6 +36,7 @@ int median()
     else
         return maxHeap.top();
 }
+
 void findMedian(int *arr, int n)
 {
     // Write your code here
@@ -47,6 +48,80 @@ void findMedian(int *arr, int n)
     }
     
 }
+
+//Approach two
+
+#include<bits/stdc++.h> 
+using namespace std; 
+void findMedian(int *arr, int n){
+    if(n==0){
+        return;
+    }
+    priority_queue<int> s; 
+    // min heap to store the greater half elements 
+    priority_queue<int,vector<int>,greater<int> > g; 
+  
+    int med = arr[0]; 
+    s.push(arr[0]); 
+  
+    cout << med<<" ";
+  
+    // reading elements of stream one by one 
+   
+    for (int i=1; i < n; i++) 
+    { 
+        int x = arr[i]; 
+  
+        // case1(left side heap has more elements) 
+        if (s.size() > g.size()) 
+        { 
+            if (x < med) 
+            { 
+                g.push(s.top()); 
+                s.pop(); 
+                s.push(x); 
+            } 
+            else
+                g.push(x); 
+  
+            med = (s.top() + g.top())/2.0; 
+        } 
+  
+        // case2(both heaps are balanced) 
+        else if (s.size()==g.size()) 
+        { 
+            if (x < med) 
+            { 
+                s.push(x); 
+                med = (int)s.top(); 
+            } 
+            else
+            { 
+                g.push(x); 
+                med = (int)g.top(); 
+            } 
+        } 
+  
+        // case3(right side heap has more elements) 
+        else
+        { 
+            if (x > med) 
+            { 
+                s.push(g.top()); 
+                g.pop(); 
+                g.push(x); 
+            } 
+            else
+                s.push(x); 
+  
+            med = (s.top() + g.top())/2.0; 
+        } 
+  
+        cout << med<<" ";
+    } 
+}
+
+
 #include <iostream>
 using namespace std;
 
